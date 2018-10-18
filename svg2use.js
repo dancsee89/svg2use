1728f8a2dbc83808f7e9c4378888aa0817801ae5
dancsee89 <d.molnar89@gmail.com>
2018-10-18 11:30:17 +0200

    fix: apply some fixes noticed while testing

--- svg2use.js
+++ svg2use.js
@@ -1,46 +1,59 @@
 const fs = require('fs');
-const domParser = require('xmldom').DOMParser;
+const xmldom = require('xmldom');
 
 async function svg2use(source = './source', target = './target') {
-    try {
-        if (fs.existsSync(source)) {
-            fs.readdir(source, (err, files) => {
-                for (let i = 0; i < files.length; i++) {
-                    const file = files[i];
-                    if (file.search('.svg') !== -1) {
-                        const fileStream = fs.readFileSync(source + '/' + file, {encoding: 'utf8'});
-                        const parsedFile = parseSvg(fileStream);
+	try {
+		if (fs.existsSync(source)) {
+			fs.readdir(source, (err, files) => {
+				for (let i = 0; i < files.length; i++) {
+					const file = files[i];
+					if (file.search('.svg') !== -1) {
+						const fileStream = fs.readFileSync(source + '/' + file, {encoding: 'utf8'});
+						const parsedFile = replaceUseTags(fileStream);
 
-                        fs.writeFile(target + '/' + file, parsedFile, (err) => {
-                            if (err)
-                                throw new Error(err);
-                        });
-                    }
-                }
-            });
-        } else {
-            throw new Error(`Source directory doesn't exist: ${source}`);
-        }
-    } catch (e) {
-        console.error(e);
-    }
+						fs.writeFile(target + '/' + file, parsedFile, (err) => {
+							if (err)
+								throw new Error(err);
+						});
+					}
+				}
+			});
+		} else {
+			throw new Error(`Source directory doesn't exist: ${source}`);
+		}
+	} catch (e) {
+		console.error(e);
+	}
 }
 
-function parseSvg(fileStream) {
-    let document = new domParser().parseFromString(fileStream);
-    const useTags = document.getElementsByTagName('use');
+function replaceUseTags(fileStream) {
+	try {
+		let document = new xmldom.DOMParser().parseFromString(fileStream);
+		const useTags = Array.from(document.getElementsByTagName('use'));
 
-    for (let i = 0; i < useTags.length; i++) {
-        let tag = useTags.item(i);
+		useTags.forEach(tag => {
+			const referencedId = tag.getAttribute('xlink:href').split('#')[1];
 
-        const referencedId = tag.getAttribute('xlink:href').split('#')[1];
+			const useTagAttributes = tag.attributes;
 
-        const referencedElement = document.getElementById(referencedId).cloneNode();
+			let referencedElement = document.getElementById(referencedId).cloneNode();
 
-        document.replaceChild(referencedElement, tag);
-    }
+			for (let j = 0; j < useTagAttributes.length; j++) {
+				const attribute = useTagAttributes.item(j);
 
-    return document;
+				if (attribute.name !== 'xlink:href') {
+					referencedElement.setAttribute(attribute.name, attribute.value);
+				}
+			}
+
+			document.replaceChild(referencedElement, tag);
+		});
+
+		return new xmldom.XMLSerializer().serializeToString(document);
+	} catch (error) {
+		console.error(`Couldn't replace use tags inside svg.`);
+		throw error;
+	}
 }
 
 module.exports = svg2use;
